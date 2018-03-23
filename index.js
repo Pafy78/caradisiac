152156553b4e9d106c8cba191bc5fb31dbb72f52
Pafy78 <pierre.baudoin@devinci.fr>
2018-03-23 15:13:11 +0100

    build API

--- index.js
+++ index.js
@@ -1,27 +1,66 @@
+const {getBrands} = require('node-car-api');
+const {getModels} = require('node-car-api');
+
 var client = require('./connection.js');
 
 const express = require('express');
 
 const app = express();
-const port = process.env.PORT || 3000;
-
-//SearchValue();
+const port = process.env.PORT || 9292;
 
 
-function SearchValue(callback){
+async function print () {
+    const brands = await getBrands();
+    var index = 0;
+    brands.forEach(async function(brand){
+        const models = await getModels(brand);
+        if(models.length != 0){
+            models.forEach(async function(model){
+                //console.log(model);
+                InsertValue(model, index);
+                index++;
+            });
+        }
+    });
+}
+function InsertValue(value, index){
+    console.log(value);
+    client.index({  
+        index: 'caradisiac',
+        id: index,
+        type: "voiture",
+        body: value
+    },function(err,resp,status) {
+        console.log(err);
+    });
+}
+
+
+function GetValue(callback){
     client.search({ 
         index: 'caradisiac',
-        type: "voiture"
+        type: "voiture", 
+        body:{
+            "from" : 0, "size" : 100,
+            "query" : {
+                "range" :{
+                    "volume" : {
+                        "gte":500
+                    }
+                }
+            },
+            
+        }
     },function (error, response,status) {
         if (error){
             console.log("search error: "+error)
         }
         else {
-            console.log("--- Response ---");
+            /*console.log("--- Response ---");
             console.log(response);
-            console.log("--- Hits ---");
+            console.log("--- Hits ---");*/
             response.hits.hits.forEach(function(hit){
-                console.log(hit);
+                //console.log(hit);
                 callback(hit);
             })
         }
@@ -29,10 +68,14 @@ function SearchValue(callback){
 }
 
 
+app.get('/populate', (req, res) => {
+    print();
+});
 
-app.get('/api/all', (req, res) => {
-    SearchValue(function(model){
-        res.write(model);
+app.get('/suv', (req, res) => {
+    res.setHeader('Content-Type', 'json');
+    GetValue(function(model){
+        res.write(JSON.stringify(model));
     });
 });
 
